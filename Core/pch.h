7628b576c18a58078ca0c8ee01705d3ef17b0de1
ChoHye0N <yunnk29@naver.com>
2026-09-07 19:02:12 +0900

    [UPDATE] vcpkg 기반 CI 구성 및 OpenCV 런타임 DLL 복사 누락 수정
    - AasCore.dll이 링크하는 OpenCV 공유 라이브러리도 UI 출력 폴더에 함께 복사하도록 CMake post-build 단계 확장
    - OpenCV include를 core/imgproc/imgcodecs로 좁혀 최소 기능 설치로도 빌드되게 함
    - Core/vcpkg.json 추가: 로컬 커스텀 OpenCV 빌드와 동일한 옵션으로 vcpkg가 OpenCV를 빌드하도록 지정
    - windows-latest 러너 내장 vcpkg로 Core를 빌드하고 UI까지 빌드해 AasCore.dll 복사 결과를 검증하는 GitHub Actions 워크플로 추가

--- Core/pch.h
+++ Core/pch.h
@@ -2,7 +2,9 @@
 #define PCH_H
 
 #include "framework.h"
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 #include <windows.h>
 
 #endif
